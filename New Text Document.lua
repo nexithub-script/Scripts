6767c308afa6f56c138573b6e3109161c38dc27f
nexithub-script <potterl0979@gmail.com>
2026-04-24 15:12:50 +0700

    Add files via upload

---
+++ New Text Document.lua
@@ -0,0 +1,25 @@
+local Players = game:GetService("Players")
+local lp = Players.LocalPlayer
+
+local StaffList = {
+    [3441625467] = "Developer", [857249396] = "Developer", [7592364667] = "Developer",
+    [5076589580] = "Developer", [4995418133] = "Developer", [5085976766] = "Developer",
+    [2210363838] = "Developer", [166836030] = "Co-Owner", [1324414468] = "Co-Owner",
+    [300706958] = "Owner", [9047416369] = "Owner"
+}
+
+local function Check(p)
+    if _G.AutoKickEnabled and StaffList[p.UserId] then
+        lp:Kick("\n[Nexit Hub]\nStaff Detected: " .. StaffList[p.UserId])
+    end
+end
+
+-- ฟังก์ชันสำหรับเช็คคนทั้งหมด (ใช้ตอนกด Toggle)
+_G.ForceCheckStaff = function()
+    for _, v in ipairs(Players:GetPlayers()) do
+        Check(v)
+    end
+end
+
+-- ตรวจจับคนจอยใหม่
+Players.PlayerAdded:Connect(Check)
